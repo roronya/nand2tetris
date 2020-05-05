b6c33ba633afe30ae4bce861646f91402f29c10a
roronya <roronya628@gmail.com>
2020-05-05 11:35:56 +0900

    chapter4

--- 04/fill/Fill.asm
+++ 04/fill/Fill.asm
@@ -6,9 +6,77 @@
 // Runs an infinite loop that listens to the keyboard input.
 // When a key is pressed (any key), the program blackens the screen,
 // i.e. writes "black" in every pixel;
-// the screen should remain fully black as long as the key is pressed. 
+// the screen should remain fully black as long as the key is pressed.
 // When no key is pressed, the program clears the screen, i.e. writes
 // "white" in every pixel;
 // the screen should remain fully clear as long as no key is pressed.
 
-// Put your code here.
+//for(int h=0;h<256;h++){
+//  for(int i=0;i<32;i++) {
+//    M[h][i]=32767;
+//    M[@SCREEN+32*h+i]
+//  }
+//}
+//
+
+// max_screen_register = 8196
+@8196
+D=A
+@max_screen_register
+M=D
+
+(LOOP)
+  // i = 0
+  @i
+  M=0
+
+  @KBD
+  D=M
+  @FILL_SCREEN_LOOP
+  D;JNE
+  @EMPTY_SCREEN_LOOP
+  D;JMP
+  (FILL_SCREEN_LOOP)
+    // current_scrreen_register = 16384 + i
+    // Memory[current_screen_register] = -1 = 111...11
+    @SCREEN
+    D=A
+    @i
+    A=D+M
+    M=-1
+
+    // while(max_screen_register -i == 0)
+    @i
+    M=M+1
+    D=M
+    @max_screen_register
+    D=M-D
+    @FILL_SCREEN_LOOP_END
+    D;JEQ
+    @FILL_SCREEN_LOOP
+    0;JMP
+  (FILL_SCREEN_LOOP_END)
+
+  (EMPTY_SCREEN_LOOP)
+    // current_scrreen_register = 16384 + i
+    // Memory[current_screen_register] = -1 = 111...11
+    @SCREEN
+    D=A
+    @i
+    A=D+M
+    M=0
+
+    // while(max_screen_register -i == 0)
+    @i
+    M=M+1
+    D=M
+    @max_screen_register
+    D=M-D
+    @EMPTY_SCREEN_LOOP_END
+    D;JEQ
+    @EMPTY_SCREEN_LOOP
+    0;JMP
+  (EMPTY_SCREEN_LOOP_END)
+(END)
+@END
+0;JMP
--- 04/mult/mult.asm
+++ 04/mult/mult.asm
@@ -6,4 +6,35 @@
 // Multiplies R0 and R1 and stores the result in R2.
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
-// Put your code here.
+// R0 * R1 をやりたい
+// R0をR1回足せばよい
+// int i = 0;
+// int sum = 0;
+// while(i < R1) { // i - R1 < 0
+//   sum += R0;
+//   i++;
+// }
+
+@i
+M=0
+@R2
+M=0
+(LOOP)
+@i
+D=M
+M=M+1 // iを読み込んだついでにインクリメントしちゃう
+@R1
+D=D-M
+@END
+D;JGE
+@R2
+D=M
+@R0
+D=D+M
+@R2
+M=D
+@LOOP
+0;JMP
+(END)
+@END
+0;JMP
