8cf8e6da12051639b4017d8d8bc8f75c7542b166
roronya <roronya628@gmail.com>
2020-08-11 17:31:57 +0900

    add commandtype

--- 07/parser/parser.go
+++ 07/parser/parser.go
@@ -1,30 +1,50 @@
 package parser
 
-import (
-	"io/ioutil"
-	"log"
-	"strings"
+type VMCommandType int
+
+const (
+	C_ARITHMETIC VMCommandType = iota
+	C_PUSH
+	C_POP
+	C_LABEL
+	C_GOTO
+	C_IF
+	C_FUNCTION
+	C_RETURN
+	C_CALL
 )
 
-type Parser struct {
-	commands [][]string
+var VMCommandTypeMap = map[string]VMCommandType{
+	"add":  C_ARITHMETIC,
+	"sub":  C_ARITHMETIC,
+	"neg":  C_ARITHMETIC,
+	"eq":   C_ARITHMETIC,
+	"gt":   C_ARITHMETIC,
+	"lt":   C_ARITHMETIC,
+	"and":  C_ARITHMETIC,
+	"or":   C_ARITHMETIC,
+	"not":  C_ARITHMETIC,
+	"push": C_PUSH,
 }
 
-func New(fileName string) *Parser {
-	vmFunc, err := ioutil.ReadFile(fileName)
-	if err != nil {
-		log.Fatal(err)
-	}
-	input := string(vmFunc)
-	rows := strings.Split(input, "\n")
-	commands := [][]string{}
-	for _, row := range rows {
-		command := []string{}
-		elements := strings.Split(row, " ")
-		for _, element := range elements {
-			command = append(command, element)
-		}
-		commands = append(commands, command)
-	}
+type Parser struct {
+	commands     [][]string
+	position     int
+	nextPosition int
+	CommandType  VMCommandType
+}
+
+func New(commands [][]string) *Parser {
 	return &Parser{commands: commands}
+}
+
+func (p *Parser) HasMoreCommands() bool {
+	return p.commands[p.nextPosition][0] != "EOF"
+}
+
+func (p *Parser) Advance() {
+	p.position = p.nextPosition
+	p.nextPosition = p.nextPosition + 1
+	command := p.commands[p.position][0]
+	p.CommandType = VMCommandTypeMap[command]
 }
--- 07/parser/parser_test.go
+++ 07/parser/parser_test.go
@@ -1,7 +1,81 @@
 package parser
 
-import "testing"
+import (
+	"testing"
+)
 
-func TestNew(t *testing.T) {
-	p := New()
+func TestHasMoreCommands(t *testing.T) {
+	commands := [][]string{
+		[]string{"push", "constant", "7"},
+		[]string{"push", "constant", "8"},
+		[]string{"add"},
+		[]string{"EOF"},
+	}
+	p := New(commands)
+	if !p.HasMoreCommands() {
+		t.Fatalf("p.HasMoreCommands is expected true. got=false.")
+	}
+	p.nextPosition = 3
+	if p.HasMoreCommands() {
+		t.Fatalf("p.HasMoreCommands is expected false. got=true.")
+	}
+}
+
+func TestHasMoreCommands_空ファイルのとき(t *testing.T) {
+	commands := [][]string{
+		[]string{"EOF"},
+	}
+	p := New(commands)
+	if p.HasMoreCommands() {
+		t.Fatalf("p.HasMoreCommands is expected false. got=true.")
+	}
+}
+
+func TestAdvance(t *testing.T) {
+	commands := [][]string{
+		[]string{"push", "constant", "7"},
+		[]string{"push", "constant", "8"},
+		[]string{"add"},
+		[]string{"EOF"},
+	}
+	p := New(commands)
+	p.Advance()
+	if !(p.position == 0 && p.nextPosition == 1) {
+		t.Fatalf("p.Advance() is expected setting position = 0, nextPosition = 1. got=position=%d, nextPosition=%d.", p.position, p.nextPosition)
+	}
+}
+
+func TestCommandType(t *testing.T) {
+	commands := [][]string{
+		[]string{"add"},
+		[]string{"sub"},
+		[]string{"neg"},
+		[]string{"eq"},
+		[]string{"gt"},
+		[]string{"lt"},
+		[]string{"and"},
+		[]string{"or"},
+		[]string{"not"},
+		[]string{"push"},
+		[]string{"EOF"},
+	}
+	expecteds := []VMCommandType{
+		C_ARITHMETIC,
+		C_ARITHMETIC,
+		C_ARITHMETIC,
+		C_ARITHMETIC,
+		C_ARITHMETIC,
+		C_ARITHMETIC,
+		C_ARITHMETIC,
+		C_ARITHMETIC,
+		C_ARITHMETIC,
+		C_PUSH,
+	}
+	p := New(commands)
+	for _, expected := range expecteds {
+		p.Advance()
+		if p.CommandType != expected {
+			t.Fatalf("p.CommandType of %v is expected %v. got=%v", p.commands[p.position][0], expected, p.CommandType)
+		}
+	}
 }
